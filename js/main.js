eeb5717ce98a0052bd8f14fd20032a15d8c46a7b
Santi Bascal <sbascalcorrea@gmail.com>
2023-10-24 17:59:31 -0300

    agregue un class para saber la primer letra y la primer palabra

--- js/main.js
+++ js/main.js
@@ -1,34 +1,68 @@
+let container = document.querySelector('.textContainer')
 
-function randomWord(wordArray, wordNum){
+
+
+function randomWord(wordArray, wordNum) {
     let randomGenerate = Math.floor(Math.random() * wordNum);
     return wordArray[randomGenerate]
 }
 
 
-function wordSelector(){
-    fetch('data/words.json')
-    .then((res)=>{
-        return res.json();
-    })
-    .then((words)=>{
-        let wordArray = words.palabras
-        let wordsNum = wordArray.length;
-        
-        document.querySelector('.textContainer').innerHTML = ''
-
-        for(let x = 0; x < 200 ; x+=1){
-            
-            document.querySelector('.textContainer').innerHTML += randomWord(wordArray, wordsNum)
-            
-        }
-
-        
-
-    })
-    .catch((err)=>{
-        console.log(err)
-    })
+function addCurrent(item, addClass){
+    item.className += ' '+addClass;
 }
+
+
+function removeCurrent(item, removeClass){
+    item.className = letter.className.replace(removeClass, '')
+}
+
+
+function divCreator(words) {
+    return `<div class="word">
+                <span class="letra">
+                    ${words.split('').join('</span><span class="letra">')}
+                </span>
+            </div>`;
+}
+
+
+
+
+
+function wordSelector() {
+    fetch('data/words.json')
+        .then((res) => {
+            return res.json();
+        })
+        .then((words) => {
+            let wordArray = words.palabras
+            let wordsNum = wordArray.length;
+
+            container.innerHTML = ''
+
+            for (let x = 0; x < 200; x += 1) {
+
+                container.innerHTML += divCreator(randomWord(wordArray, wordsNum))
+
+            }
+            
+            let classWord = document.querySelector('.word')
+            let classLetra = document.querySelector('.letra')
+
+            addCurrent(classWord, 'current')
+            addCurrent(classLetra, 'current')
+
+
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+}
+
+
+
+
 
 
 wordSelector()
