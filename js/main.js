220737b3253e45e059a5751a4cbd44fe8fbf1a5b
Santi Bascal <sbascalcorrea@gmail.com>
2023-10-26 21:15:41 -0300

    agregando funciones del juego

--- js/main.js
+++ js/main.js
@@ -1,68 +1,115 @@
-let container = document.querySelector('.textContainer')
+let container = document.querySelector('.textContainer');
 
 
 
 function randomWord(wordArray, wordNum) {
     let randomGenerate = Math.floor(Math.random() * wordNum);
-    return wordArray[randomGenerate]
+    return wordArray[randomGenerate];
 }
 
 
-function addCurrent(item, addClass){
-    item.className += ' '+addClass;
+function addClass(item, addClass) {
+    item.className += ' ' + addClass;
 }
 
 
-function removeCurrent(item, removeClass){
-    item.className = letter.className.replace(removeClass, '')
+function removeClass(item, removeClass) {
+    item.className = item.className.replace(removeClass, '');
 }
 
 
 function divCreator(words) {
-    return `<div class="word">
-                <span class="letra">
-                    ${words.split('').join('</span><span class="letra">')}
-                </span>
-            </div>`;
+    return `<div class="word"><span class="letra">${words.split('').join('</span><span class="letra">')}</span></div>`;
 }
 
 
 
 
 
-function wordSelector() {
-    fetch('data/words.json')
-        .then((res) => {
-            return res.json();
-        })
-        .then((words) => {
-            let wordArray = words.palabras
-            let wordsNum = wordArray.length;
 
-            container.innerHTML = ''
+fetch('data/words.json')
+    .then((res) => {
+        return res.json();
+    })
+    .then((words) => {
+        let wordArray = words.palabras;
+        let wordsNum = wordArray.length;
 
-            for (let x = 0; x < 200; x += 1) {
+        container.innerHTML = '';
 
-                container.innerHTML += divCreator(randomWord(wordArray, wordsNum))
+        for (let x = 0; x < 50; x += 1) {
+
+            container.innerHTML += divCreator(randomWord(wordArray, wordsNum));
+
+        }
+
+
+        let classWord = document.querySelector('.word');
+        let classLetra = document.querySelector('.letra');
+
+        addClass(classWord, 'current');
+        addClass(classLetra, 'current');
+
+
+
+    })
+
+    //CAMBIAR EL CATCH POR UN SWEETALERT O ALGO
+    .catch((err) => {
+        console.log(err);
+    })
+
+
+document.querySelector('#game').addEventListener('keyup', (evt) => {
+    let keyPress = evt.key;
+    let wordActual = document.querySelector('.word.current')
+    let letraActual = document.querySelector('.letra.current');
+    let letraEsperada = letraActual?.innerHTML || ' ';
+    let esLetra = keyPress.length === 1 && keyPress !== ' '
+    let espacio = keyPress === ' '
+
+    if (esLetra) {
+        if (letraActual) {
+            addClass(letraActual, keyPress === letraEsperada ? 'correct' : 'incorrect')
+            removeClass(letraActual, 'current')
+            if (letraActual.nextSibling) {
+                addClass(letraActual.nextSibling, 'current')
 
             }
-            
-            let classWord = document.querySelector('.word')
-            let classLetra = document.querySelector('.letra')
+        } else {
+            let letraIncorrecta = document.createElement('span');
+            letraIncorrecta.innerHTML = keyPress
+            letraIncorrecta.className = 'letra incorrect extra'
+            wordActual.appendChild(letraIncorrecta)
+        }
+    }
 
-            addCurrent(classWord, 'current')
-            addCurrent(classLetra, 'current')
+    if (espacio) {
+        if (letraEsperada !== ' ') {
+            let letrasIncorrectas = [...document.querySelectorAll('.word.current .letra:not(.correct)')]
+            letrasIncorrectas.forEach(letra => {
+                addClass(letra, 'incorrect')
+            })
+        }
+
+        removeClass(wordActual, 'current')
+        addClass(wordActual.nextSibling, 'current')
+
+        if (letraActual) {
+            removeClass(letraActual, 'current')
 
 
-        })
-        .catch((err) => {
-            console.log(err)
-        })
-}
+        }
+
+        addClass(wordActual.nextElementSibling.firstChild, 'current')
+
+    }
+
+})
 
 
 
 
 
 
-wordSelector()
+
